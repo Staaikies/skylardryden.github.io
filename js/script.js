0ef4be14d20fdf1429b95119b9c09aa5ff8ef55d
Skylar Dryden <skylar.dryden@yuppiechef.com>
2024-01-12 23:20:01 +0200

    Done section work and CSS Improvements
    Created the done css file and moved all the relevant code into it. Also worked on some JavaScript to open and close Done section.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-let headers = document.querySelectorAll(".accolade__done-header-wrapper")
+
+const windowSizeListener = () => {
+    console.log("Hello");
+
+}
+
+window.onresize = windowSizeListener;
+
+
+console.log(window.innerWidth);
+let headers = document.querySelectorAll(".done-card");
 console.log(headers);
 
 
 for (var i = 0; i < headers.length; i++) {
     headers[i].addEventListener("click", openDoneWork);
 }
-  
 
 function openDoneWork() {
-    this.classList.toggle('accolade__done-header-wrapper--open')
+    this.classList.toggle('done-card--open');
 }
