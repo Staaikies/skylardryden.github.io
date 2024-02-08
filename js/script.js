ebe2dfec0dd53c66ac59081b1a2dc379a08f01ce
Skylar <skylar.dryden@gmail.com>
2024-02-08 19:30:32 +0200

    clean up and simplify

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
-
-const windowSizeListener = () => {
-    console.log("Hello");
-
-}
-
-window.onresize = windowSizeListener;
-
-
-console.log(window.innerWidth);
+// DONE CARD OPENING AND CLOSING LOGIC
 let headers = document.querySelectorAll(".done-card");
-console.log(headers);
-
 
 for (var i = 0; i < headers.length; i++) {
     headers[i].addEventListener("click", openDoneWork);
@@ -18,4 +7,36 @@ for (var i = 0; i < headers.length; i++) {
 
 function openDoneWork() {
     this.classList.toggle('done-card--open');
+}
+
+// FORM LOGIC 
+function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var message = document.forms["contactForm"]["message"].value;
+    var error = "";
+
+    if (name == "") {
+        error += "Please enter your name.\n";
+    }
+
+    if (email == "") {
+        error += "Please enter your email address.\n";
+    } else if (!validateEmail(email)) {
+        error += "Please enter a valid email address.\n";
+    }
+
+    if (message == "") {
+        error += "Please enter a message.\n";
+    }
+
+    if (error != "") {
+        alert(error);
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
 }
