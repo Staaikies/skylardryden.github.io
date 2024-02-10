6742335982df3c80de5f1309040340ece9463760
Skylar Dryden <skylar.dryden@yuppiechef.com>
2024-02-10 14:11:48 +0200

    get contact form working basically

--- js/script.js
+++ js/script.js
@@ -9,24 +9,31 @@ function openDoneWork() {
     this.classList.toggle('done-card--open');
 }
 
+
 // FORM LOGIC 
+let form = document.querySelector(".form");
+let contactName;
+let contactEmail;
+let contactMessage;
+
+function validateEmail(email) {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
 function validateForm() {
-    var name = document.forms["contactForm"]["name"].value;
-    var email = document.forms["contactForm"]["email"].value;
-    var message = document.forms["contactForm"]["message"].value;
-    var error = "";
+    contactName = document.forms[0]["name"].value;
+    contactEmail = document.forms[0]["email"].value;
+    contactMessage = document.forms[0]["message"].value;
+    let error = "";
 
-    if (name == "") {
-        error += "Please enter your name.\n";
-    }
-
-    if (email == "") {
+    if (contactEmail == "") {
         error += "Please enter your email address.\n";
-    } else if (!validateEmail(email)) {
+    } else if (!validateEmail(contactEmail)) {
         error += "Please enter a valid email address.\n";
     }
 
-    if (message == "") {
+    if (contactMessage == "") {
         error += "Please enter a message.\n";
     }
 
@@ -36,7 +43,27 @@ function validateForm() {
     }
 }
 
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
+const sendEmail = () => {
+    console.log(contactName);
+    console.log(contactEmail);
+    console.log(contactMessage);
+
+    Email.send({
+        SecureToken : "becd85e1-4c1a-45cb-9ae7-6c76fa8b3530",
+        To : "skylar.dryden@gmail.com",
+        From : "skylar.dryden@gmail.com",
+        Subject : "A message from " + contactName,
+        Body : contactMessage + " || Their email address is: " + contactEmail
+        }).then(
+            // Replace with nicer feedback for email send.
+        message => alert(message)
+    );
 }
+
+const handleContactSubmit = (e) => {
+    e.preventDefault();
+    validateForm();
+    sendEmail();
+}
+
+form.addEventListener("submit", handleContactSubmit);
