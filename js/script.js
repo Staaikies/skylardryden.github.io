7ce29cc5b7e9480cd603c3f31fd090f21fafdb28
Skylar <skylar.dryden@gmail.com>
2024-03-11 22:00:18 +0200

    get the component card controls up and running and some responsive size changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
+// ===== CARD COMPONENT TOGGLE LOGIC
+let cardComponent = document.querySelector(".card-component");
+let colourToggle = document.querySelector(".toggle-button__checkbox--colour");
+let fontToggle = document.querySelector(".toggle-button__checkbox--font");
+
+colourToggle.addEventListener("change", () => {
+    if (cardComponent.classList.contains("card-component--light")) {
+        cardComponent.classList.remove("card-component--light");
+        cardComponent.classList.add("card-component--dark");
+    } else {
+        cardComponent.classList.remove("card-component--dark");
+        cardComponent.classList.add("card-component--light");
+    }
+});
+
+fontToggle.addEventListener("change", () => {
+    cardComponent.classList.toggle("card-component--large-font");
+});
+
 // ===== DONE CARD OPENING AND CLOSING LOGIC
 let headers = document.querySelectorAll(".done-card");
 
-const openDoneWork = () => {
-    this.classList.toggle('done-card--open');
+const openDoneWork = (event) => {
+    event.currentTarget.classList.toggle("done-card--open");
 }
 
-for (var i = 0; i < headers.length; i++) {
-    headers[i].addEventListener("click", openDoneWork);
-}
+headers.forEach(header => {
+    header.addEventListener("click", openDoneWork);
+});
 
 
 // ===== FORM LOGIC 
